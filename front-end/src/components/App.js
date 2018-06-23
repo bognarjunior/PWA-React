faf00f7df778970474baab5a91fedecb20183eed
Bognar Junior <bognar_junior@yahoo.com.br>
2018-06-23 14:54:32 -0300

    Estudos
    Criação dos primeiros componentes para estudo PWA React

---
+++ front-end/src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <h1>
+        Componente React!
+      </h1>
+    )
+  }
+}
---
+++ front-end/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import './css/pure-min.css';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
