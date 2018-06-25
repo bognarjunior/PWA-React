9925e377d94ee02bf0541766646fab6c471d8617
Bognar Junior <bognar_junior@yahoo.com.br>
2018-06-24 23:22:28 -0300

    Button
    Criação do componente Button, que será responsável pelos botões do aplicativo

---
+++ front-end/src/components/Button/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default (props) => {
+  const classes = props.principal ? 'pure-button pure-button-primary' : 'pure-button';
+  const estilo = {
+    boxSizing: 'border-box',
+    backgroundColor: props.principal ? '#2c80b9' : '#e6e6e6',
+    float: props.principal ? 'right' : 'left',
+    marginTop: '10px',
+    width: '120px',
+    height: '38px'
+  }
+
+  return (
+    <button className={classes} style={estilo} onClick={props.onClick}>
+      {props.texto}
+    </button>
+  )
+}
